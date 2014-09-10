581a823e9f36a90569dd60b5739c8ed7dedb5000
Sumit Bachketi <iwdeveloper@Sumits-Mac-Pro.local>
2014-09-10 05:10:42 -0700

    setup project

---
+++ LearnAPTiOS-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnAPTiOS-Swift
+//
+//  Created by Sumit Bachketi on 10/09/14.
+//  Copyright (c) 2014 Information Works. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
